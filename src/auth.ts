1a6bcc5a43811e35dd076a058f9c458ffa897eda
Tilak Rathoure <126514769+TilakRathoure@users.noreply.github.com>
2025-08-01 19:37:28 +0530

    add auth.js boiler plate

---
+++ src/auth.ts
@@ -0,0 +1,33 @@
+import NextAuth, { CredentialsSignin } from "next-auth"
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret:process.env.GOOGLE_CLIENT_SECRET
+    }),
+    Credentials({
+        name: "Credentials",
+        credentials:{
+            email:{
+                label:"Email",
+                type:"email",
+            },
+            password:{
+                label:"Password", type:"password"},
+        },
+        authorize: async ({email,password})=>{
+            console.log(email,password);
+
+            const user={email:"sdsd", id: "dfd"};
+
+            if(password!="passcode")
+                throw new CredentialsSignin("Password does not match");
+            else return user;
+
+        }
+    })
+  ],
+})
