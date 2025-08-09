abadbe2e897298bde5225e6c0135cb8b7d2b676b
Tilak Rathoure <126514769+TilakRathoure@users.noreply.github.com>
2025-08-09 22:47:02 +0530

    fix features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import Features from '@/components/home/Features'
 import Herosection from '@/components/home/Herosection'
-import FinueraAIHomepage from '@/components/home/Main'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <Herosection/>
-      <FinueraAIHomepage/>
+      <Features/>
     </div>
   )
 }
