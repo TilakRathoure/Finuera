5cbe32a142282f68b32a2ece737d4eb9be9c05f1
Tilak Rathoure <126514769+TilakRathoure@users.noreply.github.com>
2025-08-16 00:59:51 +0530

    add dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,215 @@
+"use client";
+
+import { Lightbulb, TrendingUp } from "lucide-react";
+import { Bar, BarChart, CartesianGrid, Line, XAxis, LineChart, Pie, PieChart } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+export const description = "A bar chart";
+
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig;
+
+
+const chartData1 = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+]
+
+const chartConfig1 = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "#1e3a8a",
+  },
+  safari: {
+    label: "Safari",
+    color: "#3b82f6", 
+  },
+  firefox: {
+    label: "Firefox",
+    color: "#60a5fa",
+  },
+  edge: {
+    label: "Edge",
+    color: "#93c5fd", 
+  },
+  other: {
+    label: "Other",
+    color: "#bfdbfe",
+  },
+} satisfies ChartConfig;
+
+
+const dashboard = () => {
+  return (
+    <div className="pt-20 flex flex-col gap-5">
+
+        <div className="flex flex-col gap-2 md:flex-row">
+
+      <Card className=" md:w-1/2">
+        <CardHeader>
+          <CardTitle>Bar Chart</CardTitle>
+          <CardDescription>January - June 2024</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <BarChart accessibilityLayer data={chartData}>
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={true}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Bar dataKey="desktop" radius={8} fill="#60a5fa" />
+            </BarChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter className="flex-col items-start gap-2 text-sm">
+          <div className="flex gap-2 leading-none font-medium">
+            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+          </div>
+          <div className="text-muted-foreground leading-none">
+            Showing total visitors for the last 6 months
+          </div>
+        </CardFooter>
+      </Card>
+
+
+        <Card className="md:w-1/2 bg-yellow-100 pt-4">
+            <CardHeader><CardTitle><Lightbulb /></CardTitle></CardHeader>
+            <CardContent>
+
+            </CardContent>
+
+        </Card>
+
+
+        </div>
+
+
+
+
+        <div className="flex flex-col gap-5 md:gap-0 md:flex-row">
+
+      <Card className="md:w-1/2">
+        <CardHeader>
+          <CardTitle>Line Chart</CardTitle>
+          <CardDescription>January - June 2024</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Line
+                dataKey="desktop"
+                type="natural"
+                stroke="var(--color-desktop)"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter className="flex-col items-start gap-2 text-sm">
+          <div className="flex gap-2 leading-none font-medium">
+            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+          </div>
+          <div className="text-muted-foreground leading-none">
+            Showing total visitors for the last 6 months
+          </div>
+        </CardFooter>
+      </Card>
+
+            <Card className="md:w-1/2 flex flex-col ">
+        <CardHeader className="items-center pb-0">
+          <CardTitle>Pie Chart - Label</CardTitle>
+          <CardDescription>January - June 2024</CardDescription>
+        </CardHeader>
+        <CardContent className="flex-1 pb-0">
+          <ChartContainer
+            config={chartConfig1}
+            className="[&_.recharts-pie-label-text]:fill-foreground mx-auto aspect-square max-h-[250px] pb-0"
+          >
+            <PieChart>
+              <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+              <Pie
+                data={chartData1}
+                dataKey="visitors"
+                label
+                nameKey="browser"
+              />
+            </PieChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter className="flex-col gap-2 text-sm">
+          <div className="flex items-center gap-2 leading-none font-medium">
+            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+          </div>
+          <div className="text-muted-foreground leading-none">
+            Showing total visitors for the last 6 months
+          </div>
+        </CardFooter>
+      </Card>
+
+        </div>
+    </div>
+  );
+};
+
+export default dashboard;
