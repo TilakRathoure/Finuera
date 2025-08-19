de769d6a6fb2890a271250ff9e2350e35897d54d
Tilak Rathoure <126514769+TilakRathoure@users.noreply.github.com>
2025-08-19 14:06:52 +0530

    fixing build error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,8 +18,6 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
-export const description = "A bar chart";
-
 const chartData = [
   { month: "January", desktop: 186 },
   { month: "February", desktop: 305 },
